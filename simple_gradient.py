7faceb4e7d78520296c81fb574bcff8fb6db818e
Tieu Long MAI <long.mai@uni.lu>
2020-02-01 17:49:59 +0100

    Update name of vairable w

--- simple_gradient.py
+++ simple_gradient.py
@@ -21,10 +21,10 @@ print('Y:', Y)
 lr = tf.constant(0.01)
 x = tf.placeholder(dtype=tf.float32, shape=[4, 3])
 y = tf.placeholder(dtype=tf.float32, shape=[4, 1])
-w1 = tf.Variable(tf.random_normal(shape=[3, 1], mean=0, stddev=0.05))
+w = tf.Variable(tf.random_normal(shape=[3, 1], mean=0, stddev=0.05))
 
 # prediction and loss
-pred = tf.matmul(x, w1)
+pred = tf.matmul(x, w)
 loss = tf.square(y - pred)
 sum_loss = tf.reduce_sum(loss)
 
@@ -34,17 +34,17 @@ sum_loss = tf.reduce_sum(loss)
 
 # # method 2
 # optimizer = tf.train.GradientDescentOptimizer(learning_rate=lr)
-# var_list = [w1]
+# var_list = [w]
 # grad_w = optimizer.compute_gradients(loss, var_list)[0]
 # grads_and_vars = zip(grad_w, var_list)
 # update_w = optimizer.apply_gradients(grads_and_vars)
 
 # method 3
-var_list = [w1]
+var_list = [w]
 grad_w = tf.gradients(loss, var_list)[0]
-update_w = w1 - lr * grad_w
-w1_update = tf.placeholder(dtype=tf.float32, shape=[3, 1])
-assign_op_w = w1.assign(w1_update)
+update_w = w - lr * grad_w
+w_update = tf.placeholder(dtype=tf.float32, shape=[3, 1])
+assign_op_w = w.assign(w_update)
 
 init = tf.global_variables_initializer()
 
@@ -52,11 +52,11 @@ with tf.Session() as sess:
     sess.run(init)
     l_all = []
     for i in range(50):
-        l, w_t = sess.run([sum_loss, update_w], feed_dict={x: X, y: Y})
+        l, u_w = sess.run([sum_loss, update_w], feed_dict={x: X, y: Y})
         l_all.append(l)
         print(l)
         # method 3
-        sess.run([assign_op_w], feed_dict={w1_update: w_t})
+        sess.run([assign_op_w], feed_dict={w_update: u_w})
 
     print("pred:", sess.run(pred, feed_dict={x: X, y: Y}))
     print("Y:", Y)
